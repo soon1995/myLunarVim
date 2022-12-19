47bdfab2bf21d83a47c5172b55b786d7fbe63977
AL SOON <topsal95@gmail.com>
2022-12-19 12:07:55 +0800

    style: enable rainbow indent line

--- lua/lvim/core/indentlines.lua
+++ lua/lvim/core/indentlines.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+
 M.config = function()
   lvim.builtin.indentlines = {
     active = true,
@@ -17,6 +18,14 @@ M.config = function()
         "Trouble",
         "text",
       },
+      char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+        "IndentBlanklineIndent5",
+        "IndentBlanklineIndent6",
+      },
       char = lvim.icons.ui.LineLeft,
       show_trailing_blankline_indent = false,
       show_first_indent_level = true,
@@ -31,6 +40,14 @@ M.setup = function()
   if not status_ok then
     return
   end
+
+  vim.opt.termguicolors = true
+  vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
 
   indent_blankline.setup(lvim.builtin.indentlines.options)
 
